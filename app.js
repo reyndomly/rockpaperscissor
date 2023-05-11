1f78b51805743afc70e2823c25e5c323198503c5
Reyndomly <reyliananareka@gmail.com>
2023-05-11 08:20:40 +0700

    add function to play the game and inject sript to html file

--- app.js
+++ app.js
@@ -40,3 +40,79 @@ function getRandomOption() {
 function isGameOver() {
     return playerScore === 5 || computerScore === 5
 }
+
+const playerScoreParam = document.getElementById("playerScore")
+const computerScoreParam = document.getElementById("computerScore")
+const infoScore = document.getElementById("infoScore")
+const playerSign = document.getElementById("playerSign")
+const computerSign = document.getElementById("computerSign")
+const btnRock = document.getElementById("btnRock")
+const btnPaper = document.getElementById("btnPaper")
+const btnScissors = document.getElementById("btnScissors")
+
+btnRock.addEventListener('click', () => handleClick('ROCK'))
+btnPaper.addEventListener('click', () => handleClick('PAPER'))
+btnScissors.addEventListener('click', () => handleClick('SCISSORS'))
+
+function handleClick(playerSelection) {
+  if (isGameOver()) {
+    alert("Game Over");
+    restartGame();
+    return
+  }
+
+  const computerSelection = getRandomOption()
+
+  playRound(playerSelection, computerSelection)
+  updateChoices(playerSelection, computerSelection)
+  updateScore()
+}
+
+function updateChoices(playerSelection, computerSelection) {
+  switch (playerSelection) {
+    case 'ROCK':
+      playerSign.textContent = '✊'
+      break
+    case 'PAPER':
+      playerSign.textContent = '✋'
+      break
+    case 'SCISSORS':
+      playerSign.textContent = '✌'
+      break
+  }
+
+  switch (computerSelection) {
+    case 'ROCK':
+      computerSign.textContent = '✊'
+      break
+    case 'PAPER':
+      computerSign.textContent = '✋'
+      break
+    case 'SCISSORS':
+      computerSign.textContent = '✌'
+      break
+  }
+}
+
+function updateScore() {
+  if (roundResult === 'TIE') {
+    infoScore.textContent = "It's a tie!"
+  } else if (roundResult === 'PLAYER') {
+    infoScore.textContent = 'You won!'
+  } else if (roundResult === 'COMPUTER') {
+    infoScore.textContent = 'You lost!'
+  }
+
+  playerScoreParam.textContent = `${playerScore}`
+  computerScoreParam.textContent = `${computerScore}`
+}
+
+function restartGame() {
+  playerScore = 0
+  computerScore = 0
+  infoScore.textContent = 'score'
+  playerScoreParam.textContent = '0'
+  computerScoreParam.textContent = '0'
+  playerSign.textContent = '🎲'
+  computerSign.textContent = '🎲'
+}
